107c2a20835bbfad04e21a74717c9e9b44d6f717
Craig Franceschetti <cfranceschetti33@rrc.ca>
2023-09-06 17:09:25 -0500

    Completed stage 2 of the assignment. NPC can now wander around randomly and will bounce away from walls.

--- Assets/Scripts/NPC/WanderBehaviour.cs
+++ Assets/Scripts/NPC/WanderBehaviour.cs
@@ -11,17 +11,16 @@ public class WanderBehaviour : MonoBehaviour
     public Vector2 directionOfWander;
     public Vector2 randomlyGeneratedPosition;
     public bool npcIsMoving = false;
-    public bool destinationReached = false;
+    public bool destinationReached = true;
+    public bool hitWall = false;
     public float npcMoveSpeed = 1f;
     public float runningTimer = 0f;
-    public float npcMoveTimer = 10f;
+    public float npcMoveTimer = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
-        npcStartingPosition = transform.position;
         runningTimer = 0f;
-        
     }
 
     // Update is called once per frame
@@ -29,23 +28,34 @@ public class WanderBehaviour : MonoBehaviour
     {
         //have my timer begin counting
         runningTimer += Time.deltaTime;
-        //get my current position
-        npcStartingPosition = transform.position;
-        //get my new location randomly
-        npcNewPosition = Random.insideUnitSphere;
-        //take apart my new location vector to direction and magnitude.
-        directionOfWander = npcNewPosition.normalized;
-        //multiply my move speed to the new location's magnitude, and store this as a new vector2
-        randomlyGeneratedPosition = directionOfWander * npcMoveSpeed;
-        //translate the normalized vector of my new location and my magnitude.
-        if(runningTimer <= 10f)
+        if (destinationReached)
+        {
+            //get new vector pointing to a random location within a sphere.
+            npcNewPosition = Random.insideUnitSphere;
+            //take apart my new location vector and just grab the direction.
+            directionOfWander = npcNewPosition.normalized;
+            //multiply my move speed to the new location's direction, and store this as a new vector2
+            randomlyGeneratedPosition = directionOfWander * npcMoveSpeed;
+            //translate the normalized vector of my new location and my magnitude.
+            destinationReached = false;
+        }
+
+        if (runningTimer <= npcMoveTimer)
         {
             transform.Translate(randomlyGeneratedPosition * Time.deltaTime);
         }
         else
         {
             runningTimer = 0f;
+            destinationReached = true;
         }
         
+        if (hitWall)
+        {
+            runningTimer = 0f;
+            destinationReached = true;
+            transform.Translate(new Vector2(0, 0) * Time.deltaTime);
+            hitWall = false;
+        }
     }
 }
