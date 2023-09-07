200c83bd990d919dee11f9527f28dfa3e406f1ea
Craig Franceschetti <cfranceschetti33@rrc.ca>
2023-09-07 16:58:48 -0500

    almost working wandering feature, as intended by assignment.

--- Assets/Scripts/NPC/WanderBehaviour.cs
+++ Assets/Scripts/NPC/WanderBehaviour.cs
@@ -6,57 +6,46 @@ using UnityEngine;
 
 public class WanderBehaviour : MonoBehaviour
 {
-    Vector2 npcStartingPosition;
-    public Vector2 npcNewPosition;
-    public Vector2 directionOfWander;
-    public Vector2 randomlyGeneratedPosition;
-    public bool npcIsMoving = false;
-    public bool destinationReached = true;
-    public bool hitWall = false;
-    public float npcMoveSpeed = 1f;
-    public float runningTimer = 0f;
-    public float npcMoveTimer = 2f;
+    public Vector2 startingPosition;
+    public Vector2 targetPosition;
+    public Vector2 vectorToTargetPosition;
+    public float npcMag;
+    public float targetMagnitude;
+    public bool hasPatrolTarget = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        runningTimer = 0f;
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //have my timer begin counting
-        runningTimer += Time.deltaTime;
-
-        if (destinationReached)
+        if(!hasPatrolTarget)
         {
-            //get new vector pointing to a random location within a sphere.
-            npcNewPosition = Random.insideUnitSphere;
-            //take apart my new location vector and just grab the direction.
-            directionOfWander = npcNewPosition.normalized;
-            //multiply my move speed to the new location's direction, and store this as a new vector2
-            randomlyGeneratedPosition = directionOfWander * npcMoveSpeed;
-            //translate the normalized vector of my new location and my magnitude.
-            destinationReached = false;
-        }
-
-        if (runningTimer <= npcMoveTimer)
-        {
-            transform.Translate(randomlyGeneratedPosition * Time.deltaTime);
+            targetPosition = Random.insideUnitCircle * 5f;
+            hasPatrolTarget = true;
         }
         else
         {
-            runningTimer = 0f;
-            destinationReached = true;
-        }
+            npcMag = startingPosition.magnitude;
+            vectorToTargetPosition = targetPosition - (Vector2)transform.position;
+            targetMagnitude = vectorToTargetPosition.magnitude;
 
-        if (hitWall)
-        {
-            runningTimer = 0f;
-            destinationReached = true;
-            transform.Translate(new Vector2(0, 0) * Time.deltaTime);
-            hitWall = false;
+            Debug.DrawRay(transform.position, targetPosition);
+
+            if (targetMagnitude >= 0.1f)
+            {
+                transform.Translate(targetPosition.normalized * Time.deltaTime);
+                
+            }
         }
+        
+
+      
+        
+       
     }
 }
