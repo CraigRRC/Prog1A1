40bd388f78165768246fa9decab226902bdf4bff
Craig Franceschetti <cfranceschetti33@rrc.ca>
2023-09-14 17:34:32 -0500

    actually got rotation working!!!! holy shit!!!!

--- Assets/Scripts/NPC/PlayerRangeDetection.cs
+++ Assets/Scripts/NPC/PlayerRangeDetection.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.AI;
+using UnityEngine.UIElements;
 
 public class PlayerRangeDetection : MonoBehaviour
 {
     public GameObject player;
     public WanderBehaviour npc;
 
+    public float rotateSpeed = 50f;
+    public float currentRotation = 0f;
+
     Vector2 playerPosition;
     Vector2 npcPosition;
-    Vector2 vectorThatIWant;
-    float moveSpeed = 3f;
-    bool chasePlayer;
+    Vector2 vectorBetweenCharacterAndNPC;
+    public float chaseRadius = 5f;
+    public float chaseSpeed = 3f;
+    public bool chasePlayer;
+    public float angle;
 
     // Start is called before the first frame update
     void Start()
@@ -25,21 +32,44 @@ public class PlayerRangeDetection : MonoBehaviour
        playerPosition = player.transform.position;
        npcPosition = transform.position;
 
-       vectorThatIWant = playerPosition - npcPosition;
-
+       vectorBetweenCharacterAndNPC = playerPosition - npcPosition;
+        
        if (chasePlayer)
        {
-            transform.Translate(vectorThatIWant.normalized * moveSpeed * Time.deltaTime);
+            //gets me the correct rotation, but I seem to be moving in world space opposed to local?
+
+            angle = Vector2.SignedAngle(Vector2.up, vectorBetweenCharacterAndNPC);
+
+            if (angle < 0f)
+            {
+                currentRotation = 0; 
+                currentRotation -= angle;
+            }
+            else if (angle > 1f)
+            {
+                currentRotation = 0;
+                currentRotation += angle;
+            }
+
+            transform.rotation = Quaternion.Euler(0, 0, angle);
+
+            
+            Debug.DrawRay(transform.position, vectorBetweenCharacterAndNPC, Color.yellow);
+            Debug.DrawRay(transform.position, transform.up, Color.green);
+            Debug.DrawRay(transform.position, Vector2.up, Color.red);
+
+            transform.Translate(vectorBetweenCharacterAndNPC.normalized * chaseSpeed * Time.deltaTime, Space.World);
+           
        }
 
-       if(vectorThatIWant.magnitude < 2f)
+       if(vectorBetweenCharacterAndNPC.magnitude < chaseRadius)
        {
-           Debug.Log("Player is within 2f");
-            chasePlayer = true;
+           chasePlayer = true;
        }
-        else
-        {
-            chasePlayer = false;
-        }
+       else
+       {
+           chasePlayer = false;
+           transform.rotation = Quaternion.Euler(0, 0, 0);
+       }
     }
 }
--- Assets/Scripts/NPC/WanderBehaviour.cs
+++ Assets/Scripts/NPC/WanderBehaviour.cs
@@ -3,48 +3,79 @@ using System.Collections.Generic;
 using System.Threading;
 using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class WanderBehaviour : MonoBehaviour
 {
     PlayerRangeDetection playerRangeDetector;
 
-    public Vector2 startingPosition;
+    public Vector2 originPosition;
+    public Vector2 lastKnownLocation;
     public Vector2 targetPosition;
     public Vector2 vectorToTargetPosition;
+    public float distanceToOrigin;
     public float npcMag;
     public float targetMagnitude;
     public bool hasPatrolTarget = false;
     public float moveSpeed = 1f;
+    public bool returnToOrigin = false;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        playerRangeDetector = GetComponent<PlayerRangeDetection>();
+        originPosition = (Vector2)transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!hasPatrolTarget)
+        if (!playerRangeDetector.chasePlayer)
         {
-            //Find a new target with a radius of 5f around the NPC.
-            targetPosition = Random.insideUnitCircle * 5f;
-            hasPatrolTarget = true;
+            if (!hasPatrolTarget)
+            {
+                //Find a new target with a radius of 5f around the NPC.
+                targetPosition = Random.insideUnitCircle * 5f;
+                hasPatrolTarget = true;
+            }
+            else
+            {
+                if (returnToOrigin)
+                {
+                    //get location where we are currently
+                    lastKnownLocation = (Vector2)transform.position;
+                    //get the direction and distance to our origin
+                    Vector2 vectorToOrigin = originPosition - lastKnownLocation;
+                    distanceToOrigin = vectorToOrigin.magnitude;
+                    //translate to this location
+                    transform.Translate(vectorToOrigin.normalized * moveSpeed * Time.deltaTime);
+                    //check to see if we've reached our origiin point.
+                    if (distanceToOrigin <= 0.05f)
+                    {
+                        //we've returned.
+                        returnToOrigin = false; 
+                    }
+                }
+                else
+                {
+                    //Make a new vector which holds the distance between our target vector, and the vector of our NPC's location.
+                    vectorToTargetPosition = targetPosition - (Vector2)transform.position;
+                    //Translate the direction of that newly acquired vector, and multiply it by our movespeed to get it's new magnitude as frame independant.
+                    transform.Translate(vectorToTargetPosition.normalized * moveSpeed * Time.deltaTime);
+                    Debug.DrawRay(transform.position, vectorToTargetPosition);
+                    //Cache the magnitude of this vector (this vector is the distance and direction from my NPC to their target location.)
+                    targetMagnitude = vectorToTargetPosition.magnitude;
+                    //Once the NPC has come close enough to it's target, we look for a new target.
+                }
+                if (targetMagnitude < 0.1f)
+                {
+                    hasPatrolTarget = false;
+                }
+            }
         }
         else
         {
-            //Make a new vector which holds the distance between our target vector, and the vector of our NPC's location.
-            vectorToTargetPosition = targetPosition - (Vector2)transform.position;
-            //Translate the direction of that newly acquired vector, and multiply it by our movespeed to get it's new magnitude as frame independant.
-            transform.Translate(vectorToTargetPosition.normalized * moveSpeed * Time.deltaTime);
-            Debug.DrawRay(transform.position, vectorToTargetPosition);
-            //Cache the magnitude of this vector (this vector is the distance and direction from my NPC to their target location.)
-            targetMagnitude = vectorToTargetPosition.magnitude;
-            //Once the NPC has come close enough to it's target, we look for a new target.
-            if(targetMagnitude < 0.1f)
-            {
-                hasPatrolTarget = false;
-            }
+            returnToOrigin = true;
         }
+       
     }
 }
